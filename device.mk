84d810c99688e99475d3cf0be29bbf58be1b15b9
rezaadi0105 <rezaadipangestu5@gmail.com>
2021-03-14 01:45:40 +0700

    whyred: build FM
    - Arrange fm blobs
    
    Signed-off-by: rezaadi0105 <rezaadipangestu5@gmail.com>

--- device.mk
+++ device.mk
@@ -213,13 +213,11 @@ PRODUCT_PACKAGES += \
     android.hardware.broadcastradio@1.0-impl
 
 # FM
-ifeq ($(BOARD_HAVE_QCOM_FM),true)
 PRODUCT_PACKAGES += \
     FM2 \
     libqcomfm_jni \
     qcom.fmradio \
     qcom.fmradio.xml
-endif
 
 # fwk-detect
 PRODUCT_PACKAGES += \
